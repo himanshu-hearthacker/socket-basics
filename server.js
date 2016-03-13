2d571324c399d90609d4cea323e8055f3e761986
himanshu-hearthacker <himkiet@gmail.com>
2016-03-13 11:24:01 +0530

    add socket.io connection to cclient and server

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ app = express();
 
 var http = require('http').Server(app);
 
+var io = require('socket.io')(http);	
+
 app.use(express.static( __dirname + '/public' ));
+
+io.on('connection', function(){
+
+console.log("user conncected via socket.io")
+
+});
 
 http.listen(PORT, function () 
 {
 	console.log("server started" + PORT);
-});
+}); 
