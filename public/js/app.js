a9111092ae4c00f33252ab553062d3d618fbd7bc
himanshu-hearthacker <himkiet@gmail.com>
2016-03-15 15:12:14 +0530

    Add front-end form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,29 @@
 var socket = io ();
 
 socket.on('connect', function () {
-	console.log('connect to socket.io server');
+	console.log('connect to socket.io server'); 
+});
+
+socket.on('message', function(message){
+	console.log('new message');
+	console.log(message.text);
+
+
+
+});
+
+// handles submitting of new messages..
+
+var $form = jQuery('#message-form');
+$form.on('submit', function(event){
+
+event.preventDefault();
+var $message =$form.find('input[name=message]')
+socket.emit('message',{
+	text : "bhbhbhjbjh",
+text : $message.val()
+
+});
+
+$message.val('');
 });
--- server.js
+++ server.js
@@ -4,17 +4,27 @@ app = express();
 
 var http = require('http').Server(app);
 
-var io = require('socket.io')(http);	
+var io = require('socket.io')(http);
 
-app.use(express.static( __dirname + '/public' ));
+app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(){
+io.on('connection', function(socket) {
+	console.log("user conncected via socket.io");
 
-console.log("user conncected via socket.io")
+	socket.on('message', function(message) {
+		console.log('message recieved' + message.text);
+		socket.broadcast.emit('message', message);
+
+	});
+
+
+	socket.emit('message', {
+		text: 'welcome to the chat application '
+
+	});
 
 });
 
-http.listen(PORT, function () 
-{
+http.listen(PORT, function() {
 	console.log("server started" + PORT);
-}); 
+});
